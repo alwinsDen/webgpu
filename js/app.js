0e4eb7ee030184ac9b0b70dacb25bfc6771500fb
alwinvarghese <jsmadesimple@gmail.com>
2024-07-27 21:54:37 +0530

    resolution controls.

--- js/app.js
+++ js/app.js
@@ -72,9 +72,21 @@ async function main(){
     pass.end();
     
     const commandBuffer = encoder.finish();
+    //this is the point of exection of comamnd buffer.
     device.queue.submit([commandBuffer]);
   }
-  render();
+  // render();
+  const observer = new ResizeObserver(entries=>{
+    for (const entry of entries) {
+      const canvas = entry.target;
+      const width = entry.contentBoxSize[0].inlineSize;
+      const height = entry.contentBoxSize[0].blockSize;
+      canvas.width = Math.max(1, Math.min(width, device.limits.maxTextureDimension2D));
+      canvas.height = Math.max(1, Math.min(height, device.limits.maxTextureDimension2D));
+      render();
+    }
+  })
+  observer.observe(canvas);
 }
 
 main();
